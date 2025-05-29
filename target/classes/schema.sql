499767700968835b64eb9b285176fa1f2b27dbb9
gsuscastellanosSC <53982913+gsuscastellanosSC@users.noreply.github.com>
2025-05-29 02:26:29 -0500

    Delete target directory

--- target/classes/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE restaurantes (
-                              id BIGINT PRIMARY KEY AUTO_INCREMENT,
-                              nombre VARCHAR(100) NOT NULL,
-                              nit VARCHAR(20) NOT NULL,
-                              direccion VARCHAR(255) NOT NULL,
-                              telefono VARCHAR(20) NOT NULL,
-                              urllogo VARCHAR(255) NOT NULL,
-                              id_propietario BIGINT NOT NULL
-);
